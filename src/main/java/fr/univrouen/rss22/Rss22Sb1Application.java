ea04f9bcb8ea87f9a1553094fd0ba8a49afcefaa
jerradi <jerradisafae12@gmail.com>
2022-04-04 10:10:26 +0200

    premier commit

---
+++ src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
@@ -0,0 +1,15 @@
+package fr.univrouen.rss22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss22Sb1Application {
+
+	public static void main(String[] args) {
+		System.getProperties().put("server.port", 8100);
+		SpringApplication.run(Rss22Sb1Application.class, args);
+		
+	}
+
+}
