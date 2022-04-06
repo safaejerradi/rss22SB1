f03b9a74a64310ef320ab8a22f6cb849a39ecc69
jerradi <jerradisafae12@gmail.com>
2022-04-06 08:52:32 +0200

    port 8080

--- src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
+++ src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Rss22Sb1Application {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 8100);
+		//System.getProperties().put("server.port", 8100);
 		SpringApplication.run(Rss22Sb1Application.class, args);
 		
 	}
